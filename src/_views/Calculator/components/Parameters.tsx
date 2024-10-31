0d355e5374bb81d1563eeebb711db2baf2127350
Bartosz <bartosz.janiuk@tsh.io>
2024-10-31 14:59:53 +0100

    adjust cookie section

--- src/_views/Calculator/components/Parameters.tsx
+++ src/_views/Calculator/components/Parameters.tsx
@@ -10,100 +10,100 @@ const REQUIRED_ERROR_MESSAGE = "Pole jest wymagane";
 const MAX_LIMIT_ERROR_MESSAGE = "Przekroczono limit";
 
 const schema = z.object({
-  weight: z
-    .number({
-      required_error: REQUIRED_ERROR_MESSAGE,
-      invalid_type_error: REQUIRED_ERROR_MESSAGE,
-    })
-    .min(1, REQUIRED_ERROR_MESSAGE)
-    .max(300, MAX_LIMIT_ERROR_MESSAGE),
-  height: z
-    .number({
-      required_error: REQUIRED_ERROR_MESSAGE,
-      invalid_type_error: REQUIRED_ERROR_MESSAGE,
-    })
-    .min(1, REQUIRED_ERROR_MESSAGE)
-    .max(250, MAX_LIMIT_ERROR_MESSAGE),
-  age: z
-    .number({
-      required_error: REQUIRED_ERROR_MESSAGE,
-      invalid_type_error: REQUIRED_ERROR_MESSAGE,
-    })
-    .min(1, REQUIRED_ERROR_MESSAGE)
-    .max(100, MAX_LIMIT_ERROR_MESSAGE),
+	weight: z
+		.number({
+			required_error: REQUIRED_ERROR_MESSAGE,
+			invalid_type_error: REQUIRED_ERROR_MESSAGE,
+		})
+		.min(1, REQUIRED_ERROR_MESSAGE)
+		.max(300, MAX_LIMIT_ERROR_MESSAGE),
+	height: z
+		.number({
+			required_error: REQUIRED_ERROR_MESSAGE,
+			invalid_type_error: REQUIRED_ERROR_MESSAGE,
+		})
+		.min(1, REQUIRED_ERROR_MESSAGE)
+		.max(250, MAX_LIMIT_ERROR_MESSAGE),
+	age: z
+		.number({
+			required_error: REQUIRED_ERROR_MESSAGE,
+			invalid_type_error: REQUIRED_ERROR_MESSAGE,
+		})
+		.min(1, REQUIRED_ERROR_MESSAGE)
+		.max(100, MAX_LIMIT_ERROR_MESSAGE),
 });
 
 type FormData = z.infer<typeof schema>;
 
 export const Parameters = ({ onNextStep }: { onNextStep: VoidFunction }) => {
-  const { navigationData, updateNavigationData } = useCalculatorData();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm<FormData>({
-    defaultValues: {
-      age: navigationData.age || undefined,
-      height: navigationData.height || undefined,
-      weight: navigationData.weight || undefined,
-    },
-    mode: "all",
-    resolver: zodResolver(schema),
-  });
+	const { navigationData, updateNavigationData } = useCalculatorData();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isValid },
+	} = useForm<FormData>({
+		defaultValues: {
+			age: navigationData.age || undefined,
+			height: navigationData.height || undefined,
+			weight: navigationData.weight || undefined,
+		},
+		mode: "all",
+		resolver: zodResolver(schema),
+	});
 
-  const onSubmit: SubmitHandler<FormData> = (data) => {
-    if (Object.values(data).some((value) => !value)) return;
-    updateNavigationData({
-      age: data.age,
-      height: data.height,
-      weight: data.weight,
-    });
-    onNextStep();
-  };
-  return (
-    <div className="flex flex-col gap-8 text-center w-full md:min-w-[600px]">
-      <h5 className="text-3xl">Twoje parametry</h5>
-      <form className="w-full">
-        <Input
-          required
-          errors={errors}
-          label="Waga (kg)"
-          name="weight"
-          id="weight"
-          type="number"
-          rules={{ valueAsNumber: true }}
-          register={register}
-        />
-        <Input
-          required
-          errors={errors}
-          label="Wzrost (cm)"
-          name="height"
-          id="height"
-          type="number"
-          rules={{ valueAsNumber: true }}
-          register={register}
-        />
-        <Input
-          required
-          errors={errors}
-          label="Wiek"
-          name="age"
-          id="age"
-          type="number"
-          rules={{ valueAsNumber: true }}
-          register={register}
-        />
-      </form>
+	const onSubmit: SubmitHandler<FormData> = (data) => {
+		if (Object.values(data).some((value) => !value)) return;
+		updateNavigationData({
+			age: data.age,
+			height: data.height,
+			weight: data.weight,
+		});
+		onNextStep();
+	};
+	return (
+		<div className="flex flex-col gap-8 text-center w-full md:min-w-[600px] max-w-2xl">
+			<h5 className="text-3xl">Twoje parametry</h5>
+			<form className="w-full">
+				<Input
+					required
+					errors={errors}
+					label="Waga (kg)"
+					name="weight"
+					id="weight"
+					type="number"
+					rules={{ valueAsNumber: true }}
+					register={register}
+				/>
+				<Input
+					required
+					errors={errors}
+					label="Wzrost (cm)"
+					name="height"
+					id="height"
+					type="number"
+					rules={{ valueAsNumber: true }}
+					register={register}
+				/>
+				<Input
+					required
+					errors={errors}
+					label="Wiek"
+					name="age"
+					id="age"
+					type="number"
+					rules={{ valueAsNumber: true }}
+					register={register}
+				/>
+			</form>
 
-      <Button
-        disabled={!isValid}
-        type="submit"
-        onClick={handleSubmit(onSubmit)}
-        className="self-center"
-      >
-        Następny krok
-      </Button>
-    </div>
-  );
+			<Button
+				disabled={!isValid}
+				type="submit"
+				onClick={handleSubmit(onSubmit)}
+				className="self-center"
+			>
+				Następny krok
+			</Button>
+		</div>
+	);
 };
--- src/components/Cookie/Cookie.tsx
+++ src/components/Cookie/Cookie.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const Cookie = () => {
 	const [cookie, setCookie] = useState("");
-	const [isAvaliable, setIsAvaliable] = useState(true);
+	const [isAvailable, setIsAvaliable] = useState(true);
 
 	useEffect(() => {
 		const cookie = localStorage.getItem("cookie");
@@ -21,14 +21,24 @@ export const Cookie = () => {
 
 	return (
 		<>
-			{!cookie && !isAvaliable ? (
-				<div className="fixed bottom-0 left-0 w-full bg-softGray z-[1000] shadow-md flex items-center px-6 py-40">
-					<div>
-						Ta strona wykorzystuje pliki cookies (tzw. ciasteczka). Kliknij
-						<button onClick={handleSetCookie}>Akceptuj</button>, aby ta
-						informacja nie pojawiała się więcej. Kliknij{" "}
-						<a href="/polityka-cookies">Polityka Cookies</a>, aby dowiedzieć się
-						więcej.
+			{!cookie && !isAvailable ? (
+				<div className="fixed bottom-0 left-0 w-full bg-softGray z-[1000] shadow-md flex items-center px-6 py-4">
+					<div className="w-full flex items-center container responsive-padding mx-auto gap-4">
+						<img src="/assets/cookie.svg" alt="Cookie" className="w-12" />
+						<div className=" w-fit">
+							Ta strona wykorzystuje pliki cookies (tzw. ciasteczka). Kliknij{" "}
+							<button
+								className="text-primary font-medium"
+								onClick={handleSetCookie}
+							>
+								Akceptuj
+							</button>
+							, aby ta informacja nie pojawiała się więcej. Kliknij{" "}
+							<a className="text-primary underline" href="/polityka-cookies">
+								Polityka Cookies
+							</a>
+							, aby dowiedzieć się więcej.
+						</div>
 					</div>
 				</div>
 			) : (
