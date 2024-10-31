cfc876b8b7637c132639629f132fcff2ec185b20
Bartosz <bartosz.janiuk@tsh.io>
2024-10-31 10:35:13 +0100

    fix candidate for calculator height

--- src/_views/Calculator/components/Goal.tsx
+++ src/_views/Calculator/components/Goal.tsx
@@ -8,63 +8,63 @@ import { goal } from "../constants/calculator";
 import { useCalculatorData } from "../context/Calculator";
 
 const schema = z.object({
-  goal: z.string().min(1, "Wybierz cel").trim(),
+	goal: z.string().min(1, "Wybierz cel").trim(),
 });
 
 type FormData = z.infer<typeof schema>;
 
 export const Goal = ({ onNextStep }: { onNextStep: VoidFunction }) => {
-  const { navigationData, updateNavigationData } = useCalculatorData();
+	const { navigationData, updateNavigationData } = useCalculatorData();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm<FormData>({
-    defaultValues: { goal: navigationData.goal },
-    resolver: zodResolver(schema),
-  });
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isValid },
+	} = useForm<FormData>({
+		defaultValues: { goal: navigationData.goal },
+		resolver: zodResolver(schema),
+	});
 
-  const onSubmit: SubmitHandler<FormData> = (data) => {
-    if (!data.goal) return;
-    updateNavigationData({ goal: data.goal });
-    onNextStep();
-  };
+	const onSubmit: SubmitHandler<FormData> = (data) => {
+		if (!data.goal) return;
+		updateNavigationData({ goal: data.goal });
+		onNextStep();
+	};
 
-  return (
-    <div className="flex flex-col gap-8 text-center max-w-2xl">
-      <h5 className="text-3xl">Jaki jest Twój cel?</h5>
-      <form className="w-full md:min-w-[600px]">
-        <ul className="flex flex-col gap-2 list-none">
-          <RadioButton
-            register={register}
-            name="goal"
-            label="chcę schudnąć"
-            value={goal.lose}
-          />
-          <RadioButton
-            register={register}
-            name="goal"
-            label="chcę utrzymać masę ciała"
-            value={goal.hold}
-          />
-          <RadioButton
-            register={register}
-            name="goal"
-            label="chcę przytyć"
-            value={goal.gain}
-          />
-        </ul>
-      </form>
-      <Button
-        isWithoutAnimation
-        disabled={!isValid}
-        type="submit"
-        onClick={handleSubmit(onSubmit)}
-        className="self-center"
-      >
-        Następny krok
-      </Button>
-    </div>
-  );
+	return (
+		<div className="flex flex-col gap-8 text-center max-w-2xl h-full">
+			<h5 className="text-3xl">Jaki jest Twój cel?</h5>
+			<form className="w-full md:min-w-[600px]">
+				<ul className="flex flex-col gap-2 list-none">
+					<RadioButton
+						register={register}
+						name="goal"
+						label="chcę schudnąć"
+						value={goal.lose}
+					/>
+					<RadioButton
+						register={register}
+						name="goal"
+						label="chcę utrzymać masę ciała"
+						value={goal.hold}
+					/>
+					<RadioButton
+						register={register}
+						name="goal"
+						label="chcę przytyć"
+						value={goal.gain}
+					/>
+				</ul>
+			</form>
+			<Button
+				isWithoutAnimation
+				disabled={!isValid}
+				type="submit"
+				onClick={handleSubmit(onSubmit)}
+				className="self-center"
+			>
+				Następny krok
+			</Button>
+		</div>
+	);
 };
